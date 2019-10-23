00e98194417c6cc0b2d5a512b046344d04740b96
BlueWorldInc <ocalnuh@gmail.com>
2019-10-23 17:01:35 +0200

    [add] Added a simple route and tested a simple component in component in a html page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { UserComponent } from './user/user.component';
+import { PatientComponent } from './user/patient/patient.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'patient', component: PatientComponent
+  },
+  {
+    path: 'user', component: UserComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
