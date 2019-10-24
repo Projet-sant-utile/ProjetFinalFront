698984d279378917e178cc7c61274d9965a9d41a
Amanda MOMPO <amanda.mompo@outlook.fr>
2019-10-24 16:40:51 +0200

    [Add] Leaflet

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+/* import { MapComponent } from './map.component'; */
+/* import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -12,4 +13,29 @@ export class MapComponent implements OnInit {
   ngOnInit() {
   }
 
+} */
+
+// Ajouter OnInit pour effectuer des opérations à l'initialisation du composant.
+import { Component, OnInit } from '@angular/core';
+
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+// Implémenter OnInit
+export class MapComponent implements OnInit {
+
+  // Fonction d'initialisation du composant.
+  ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('frugalmap').setView([46.7311634, 3.0599573], 6);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
+  }
 }
