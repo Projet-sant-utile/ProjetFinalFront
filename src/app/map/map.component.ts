da95ca09d3d9abd01bcb3757b9e2e85b608e674f
Amanda MOMPO <amanda.mompo@outlook.fr>
2019-10-25 13:08:53 +0200

    [Add] Markers, radius

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,21 +1,3 @@
-/* import { MapComponent } from './map.component'; */
-/* import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
-})
-export class MapComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-} */
-
-// Ajouter OnInit pour effectuer des opérations à l'initialisation du composant.
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
@@ -34,6 +16,12 @@ export class MapComponent implements OnInit {
   lonSearch: number;
   myfrugalmap: any;
   urlAddress: any;
+  myIcon = L.icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png',
+    iconSize: [25, 41], // size of the icon
+    iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -41] // point from which the popup should open relative to the iconAnchor 
+  });
 
   constructor(private http: HttpClient) {
   }
@@ -76,15 +64,14 @@ export class MapComponent implements OnInit {
 
       // Do something after
       this.myPrint2();
+      this.myMarkers();
+      this.mapCalcDistance(56, 6, 57, 5);
+      L.circle([this.latSearch, this.lonSearch], { radius: 20000 }).addTo(this.myfrugalmap);
     })();
   }
 
   myPrint2() {
-    const myIcon = L.icon({
-      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
-    });
-
-    L.marker([this.latSearch, this.lonSearch], { icon: myIcon })
+    L.marker([this.latSearch, this.lonSearch], { icon: this.myIcon })
       .bindPopup('Vous êtes ici').addTo(this.myfrugalmap).openPopup();
 
 
@@ -94,4 +81,32 @@ export class MapComponent implements OnInit {
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
+
+  myMarkers() {
+    L.marker([50.1311634, 3.8599573], { icon: this.myIcon }).addTo(this.myfrugalmap).openPopup();
+
+    L.marker([50.5611634, 3.569785], { icon: this.myIcon }).addTo(this.myfrugalmap).openPopup();
+
+  }
+
+  mapCalcDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2)
+      ;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
+    console.log(d);
+    return d;
+
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI / 180)
+  }
+
 }
