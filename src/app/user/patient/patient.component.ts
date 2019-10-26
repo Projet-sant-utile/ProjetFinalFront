9a3723a9703c8a4b714cec520d689e0f10550793
Hazrian <chazotflorian@gmail.com>
2019-10-26 15:28:16 +0200

    [add] link front to db for patient form

--- src/app/user/patient/patient.component.ts
+++ src/app/user/patient/patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+  navigate() {
+
+    this.router.navigate(['/patient/ajout']);
+
+
+
+  }
+
 
 }
